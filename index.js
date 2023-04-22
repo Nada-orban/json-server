20ee39a546fb78bee44319f63bc81e129efaab68
nada <nadasamir9334@gmail.com>
2023-04-22 02:08:08 -0400

    save

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    console.log("Request: " + req.method + " to " + req.url);
+    res.writeHead(200, "OK");
+    res.write("<h1>Hello</h1>Node.js is working");
+    res.end();
+}).listen(5454);
+console.log("Ready on port 5454");
